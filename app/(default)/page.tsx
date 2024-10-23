56f8a89bd6eac1f73c9dcde199f6045ac5fb91ff
yiroaalmly04 <seven123.max@gmail.com>
2024-10-23 17:21:17 +0800

    ✨ feat: update page

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { useModal } from "@/components/modals/providers";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const products = [
   {
@@ -70,29 +77,35 @@ const products = [
   // More products...
 ];
 
+<div className=""></div>;
+
 export default function Example() {
   const { openModal } = useModal();
 
   return (
-    <div className="">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
-          {products.map((product) => (
-            <div key={product.id} className="group" onClick={openModal}>
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
+        {products.map((product) => (
+          <Card key={product.id} className="group" onClick={openModal}>
+            <CardHeader>
+              <CardTitle>{product.name}</CardTitle>
+              <CardDescription className="text-sm text-muted-foreground font-bold">
+                {product.price}
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="p-3 pt-0">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 shadow-xl hover:bg-gray-200">
                 <img
                   alt={product.imageAlt}
                   src={product.imageSrc}
-                  className="object-cover object-center group-hover:opacity-75 size-72"
+                  className="object-cover object-center group-hover:opacity-75 "
                 />
               </div>
-              <h3 className="mt-4 text-sm ">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium ">{product.price}</p>
-            </div>
-          ))}
-        </div>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
